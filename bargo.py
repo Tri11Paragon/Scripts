dd5f3e72459216f32c81752e18aff72ca603d0e1
Brett Laptop <tri11paragon@tpgc.me>
2024-05-28 20:18:50 -0400

    silly little bargo

--- bargo.py
+++ bargo.py
@@ -6,5 +6,17 @@ import requests
 import json
 import argparse
 import os
+import subprocess
 import create_git_repo as repo
 
+def open_process(command):
+    process = subprocess.Popen(command, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    exit_code = process.wait()
+    #print(stdout, stderr, exit_code)
+    return (stdout, stderr, exit_code)
+
+cmake_output, cmake_err, _ = open_process(["cmake", "--version"])
+
+cmake_lines = cmake_output.splitlines()
+print(str(cmake_lines[0]).split("version ")[1][0:-1])

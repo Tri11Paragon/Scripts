c0504863b238a6920e5ba39620de6f65d32e1933
Brett Laptop <tri11paragon@tpgc.me>
2025-06-28 22:36:49 -0400

    create models

--- news/sync.sh
+++ news/sync.sh
@@ -1,10 +1,10 @@
 #!bash
-rsync -a models/ brett@index:~/models
+rsync -a models brett@index:~/
 
 for f in models/*.modelfile; do
 	echo "Updating model: $f"
 	filename="${f##*/}"
 	basename="${filename%.*}"
 	echo "Model name: $basename from $filename"
-	ssh brett@index "ollama rm $basename && ollama create $basename -f \~/$f"
+	ssh brett@index "ollama rm $basename & ollama create $basename -f '$f'"
 done
